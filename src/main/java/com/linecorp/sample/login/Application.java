6aa09b457fb915a7a08f96a111ec951feea08491
boozikaa <khomsan.cng@gmail.com>
2017-03-31 18:35:22 +0700

    Update Application.java

--- src/main/java/com/linecorp/sample/login/Application.java
+++ src/main/java/com/linecorp/sample/login/Application.java
@@ -28,3 +28,12 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
+@Bean
+public FilterRegistrationBean filterRegistrationBean() {
+    FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+    characterEncodingFilter.setEncoding("UTF-8");
+    registrationBean.setFilter(characterEncodingFilter);
+    return registrationBean;
+}
